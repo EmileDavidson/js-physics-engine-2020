c6475fd8e0d3e1923d100035a397618fa6eb9b1a
EmileDavidson <Emiledavidson395@gmail.com>
2020-03-07 15:14:57 +0100

    verandering aangebracht? volgens git

--- html/DraggableSystem/script.js
+++ html/DraggableSystem/script.js
@@ -14,76 +14,84 @@ let circles = [];
 let colors = ["Red", "Green", "Blue", "Orange", "Purple"];
 //Maakt alle circles aan en zet ze in een array
 CreateCircles();
+
 function CreateCircles() {
-    for (let i = 0; i < 50; i++) {
-        //    let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(20, 50), GetRandomColor(), true);
-        let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(20, 50), "Red", false);
-        circles.push(circle);
-    }
-    for (let i = 0; i < 50; i++) {
-        //    let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(20, 50), GetRandomColor(), true);
-        let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(20, 50), "Green", true);
-        circles.push(circle);
-    }
+  for (let i = 0; i < 10; i++) {
+    let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), 10, "Green", true);
+    circles.push(circle);
+  }
 }
 
 //Update alle circles wanneer nodig.
 function UpdateCircles() {
-    for (let i = 0; i < circles.length; i++) {
-        if (circles[i].drag == true) {
-            circles[i].x = mousePosX;
-            circles[i].y = mousePosY;
-        }
+  for (let i = 0; i < circles.length; i++) {
+    if (circles[i].drag == true) {
+      circles[i].x = mousePosX;
+      circles[i].y = mousePosY;
     }
+  }
 }
 
 //tekent alle circles
 function DrawCircles() {
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].draw(context);
-    }
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].draw(context);
+    drawLinesBetweenPoints();
+  }
 }
 
 //update word elke 10 milli seconde uitgevoert en kijkt of er dingen moeten veranderen en drawed alles opnieuw.
 Update();
+
 function Update() {
-    context.clearRect(0,0,width,height);
-    UpdateCircles();
-    DrawCircles();
+  context.clearRect(0, 0, width, height);
+  UpdateCircles();
+  DrawCircles();
 }
 setInterval(Update, 10);
 
 //onmouse move save the location of my mouse
-document.onmousemove = function (e) {
-    mousePosX = e.pageX;
-    mousePosY = e.pageY;
+document.onmousemove = function(e) {
+  mousePosX = e.pageX;
+  mousePosY = e.pageY;
 };
 //on mouse down check if its on a circle an set it to draggable
-document.onmousedown = function (e) {
-    for (let i = 0; i < circles.length; i++) {
-        if(circles[i].draggable == true) {
-            if(e.pageY > circles[i].y - circles[i].radius && e.pageY < circles[i].y + circles[i].radius){
-                if(e.pageX > circles[i].x - circles[i].radius && e.pageX < circles[i].x + circles[i].radius){
-                    circles[i].drag = true;
-                    return;
-                }
-            }
+document.onmousedown = function(e) {
+  for (let i = 0; i < circles.length; i++) {
+    if (circles[i].draggable == true) {
+      if (e.pageY > circles[i].y - circles[i].radius && e.pageY < circles[i].y + circles[i].radius) {
+        if (e.pageX > circles[i].x - circles[i].radius && e.pageX < circles[i].x + circles[i].radius) {
+          circles[i].drag = true;
+          return;
         }
+      }
     }
+  }
 };
 //on mouse up check if there is a circle that gets dragged an set is back to false
-document.onmouseup = function (e) {
-    for (let i = 0; i < circles.length; i++) {
-        if(circles[i].drag == true){
-            circles[i].drag = false;
-        }
+document.onmouseup = function(e) {
+  for (let i = 0; i < circles.length; i++) {
+    if (circles[i].drag == true) {
+      circles[i].drag = false;
     }
+  }
 };
 
-//Simple functies for getting things.
-function GetRandomColor() {
-    let number = getRandomInt(0, colors.length);
-    return colors[number];
+
+function drawLinesBetweenPoints() {
+
+  context.beginPath();
+  context.lineWidth = 1;
+  for (let i = 0; i < circles.length; i++) {
+    context.lineTo(circles[i].x, circles[i].y);
+  }
+  context.closePath();
+  context.stroke();
 }
 
 
+//Simple functies for getting things.
+function GetRandomColor() {
+  let number = getRandomInt(0, colors.length);
+  return colors[number];
+}
