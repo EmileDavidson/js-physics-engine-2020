9823b2893906ae5fab1d03dadd2b0e60fc7cea59
EmileDavidson <Emiledavidson395@gmail.com>
2020-03-19 13:05:11 +0100

    draggable stystem verbeterd

--- html/DraggableSystem/script.js
+++ html/DraggableSystem/script.js
@@ -17,65 +17,26 @@ CreateCircles();
 
 function CreateCircles() {
   for (let i = 0; i < 10; i++) {
-    let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), 10, "Green", true);
+    let circle = new Point(getRandomInt(0, width), getRandomInt(0, height), getRandomInt(30,50), GetRandomColor(), true);
     circles.push(circle);
-  }
-}
-
-//Update alle circles wanneer nodig.
-function UpdateCircles() {
-  for (let i = 0; i < circles.length; i++) {
-    if (circles[i].drag == true) {
-      circles[i].x = mousePosX;
-      circles[i].y = mousePosY;
-    }
-  }
-}
-
-//tekent alle circles
-function DrawCircles() {
-  for (let i = 0; i < circles.length; i++) {
-    circles[i].draw(context);
-    drawLinesBetweenPoints();
   }
 }
 
 //update word elke 10 milli seconde uitgevoert en kijkt of er dingen moeten veranderen en drawed alles opnieuw.
 Update();
-
 function Update() {
   context.clearRect(0, 0, width, height);
-  UpdateCircles();
   DrawCircles();
+  drawLinesBetweenPoints();
 }
 setInterval(Update, 10);
 
-//onmouse move save the location of my mouse
-document.onmousemove = function(e) {
-  mousePosX = e.pageX;
-  mousePosY = e.pageY;
-};
-//on mouse down check if its on a circle an set it to draggable
-document.onmousedown = function(e) {
-  for (let i = 0; i < circles.length; i++) {
-    if (circles[i].draggable == true) {
-      if (e.pageY > circles[i].y - circles[i].radius && e.pageY < circles[i].y + circles[i].radius) {
-        if (e.pageX > circles[i].x - circles[i].radius && e.pageX < circles[i].x + circles[i].radius) {
-          circles[i].drag = true;
-          return;
-        }
-      }
-    }
+function DrawCircles(){
+  for(let i = 0; i < circles.length; i++){
+    circles[i].draw();
   }
-};
-//on mouse up check if there is a circle that gets dragged an set is back to false
-document.onmouseup = function(e) {
-  for (let i = 0; i < circles.length; i++) {
-    if (circles[i].drag == true) {
-      circles[i].drag = false;
-    }
-  }
-};
+}
+
 
 
 function drawLinesBetweenPoints() {
